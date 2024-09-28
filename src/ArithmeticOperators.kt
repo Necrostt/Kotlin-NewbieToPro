214f1482e0f09f174d94b9bea0287bdaad6bb60e
Euller_Gabriel <eullergoliveira@gmail.com>
2024-09-28 09:26:10 +0100

    Variable Types and Arithmetic Operators

---
+++ src/ArithmeticOperators.kt
@@ -0,0 +1,10 @@
+
+
+
+fun main(){
+
+    val x = 15
+    val y = 7
+    val result = x + y
+    println(result)
+}
---
+++ src/VariableTypes.kt
@@ -0,0 +1,12 @@
+
+
+fun main(){
+
+    //Var(Variable): Content of var variables is mutable
+    //Val(Value): Content of val values are not mutable
+    var myVariable = 5
+
+    println("The value of the variable is $myVariable")
+    myVariable = 1
+    println("The value is now: $myVariable")
+}
